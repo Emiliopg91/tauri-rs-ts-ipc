f30211bb66c730f5173bedaf865633903a044323
Emiliopg91 <ojosdeserbio@gmail.com>
2026-06-24 12:04:26 +0200

    Backend dir creation

--- src/lib.rs
+++ src/lib.rs
@@ -126,6 +126,10 @@ pub fn inner_build(
     println!("cargo:warning=  Found {} events", events.len());
     println!("cargo:warning=  Found {} structs", used_structs.len());
 
+    if !fs::exists(&backend_dir).unwrap() {
+        fs::create_dir_all(&backend_dir).unwrap();
+    }
+
     println!(
         "cargo:warning=Generating models file in '{}'",
         models_path
@@ -161,114 +165,4 @@ pub fn inner_build(
     events.sort_by_key(|e| e.name.clone());
     EventDefinition::generate_file(listener_path, events);
     println!("cargo:warning=  Done");
-
-    /*
-    if !fs::exists(&backend_dir).unwrap() {
-        fs::create_dir_all(&backend_dir).unwrap();
-    }
-
-    let mut files = Vec::new();
-    find_rs_files(&src_tauri_path, &mut files).unwrap();
-
-    let mut commands = Vec::new();
-    for file in &files {
-        for cmd in CommandDefinition::find(file, &src_tauri_path) {
-            commands.push(cmd);
-        }
-    }
-
-    println!("cargo:warning=Looking for events...");
-    let mut events = Vec::new();
-    for file in &files {
-        for event in EventDefinition::find(file, &src_tauri_path) {
-            events.push(event);
-        }
-    }
-    println!("cargo:warning=  Found {} events", events.len());
-
-    println!("cargo:warning=Looking for structs...");
-    let mut files_for_structs: HashMap<String, Vec<String>> = HashMap::new();
-    for cmd in &commands {
-        for pat in cmd.get_inner_leafs() {
-            let struct_name = pat.split("::").last().unwrap_or(&pat);
-
-            if let Some(path) = pat.strip_prefix("crate::") {
-                let path = PathBuf::from(&src_tauri_path)
-                    .join(path.replace("::", "/"))
-                    .parent()
-                    .unwrap()
-                    .to_path_buf();
-                let mut path_str = format!("{}.rs", path.display());
-                if !fs::exists(&path_str).unwrap() {
-                    path_str = format!("{}/mod.rs", path.display());
-                }
-
-                let entry = files_for_structs.entry(path_str).or_default();
-                entry.push(struct_name.to_string());
-            }
-        }
-    }
-    for event in &events {
-        for pat in event.get_inner_leafs() {
-            let struct_name = pat.split("::").last().unwrap_or(&pat);
-
-            if let Some(path) = pat.strip_prefix("crate::") {
-                let path = PathBuf::from(&src_tauri_path)
-                    .join(path.replace("::", "/"))
-                    .parent()
-                    .unwrap()
-                    .to_path_buf();
-                let mut path_str = format!("{}.rs", path.display());
-                if !fs::exists(&path_str).unwrap() {
-                    path_str = format!("{}/mod.rs", path.display());
-                }
-
-                let entry = files_for_structs.entry(path_str).or_default();
-                entry.push(struct_name.to_string());
-            }
-        }
-    }
-
-    let mut structs = Vec::new();
-    let mut already_added = HashSet::new();
-    loop {
-        let mut new_structs = Vec::new();
-        for (file, structs_to_find) in &files_for_structs {
-            for (name, def) in StructDefinition::find(file, &src_tauri_path) {
-                if structs_to_find.contains(&name) && already_added.insert(def.name.clone()) {
-                    new_structs.push(def);
-                }
-            }
-        }
-
-        files_for_structs.clear();
-        for struct_d in &new_structs {
-            for pat in struct_d.get_inner_leafs() {
-                let struct_name = pat.split("::").last().unwrap_or(&pat);
-
-                if let Some(path) = pat.strip_prefix("crate::") {
-                    let path = PathBuf::from(&src_tauri_path)
-                        .join(path.replace("::", "/"))
-                        .parent()
-                        .unwrap()
-                        .to_path_buf();
-                    let mut path_str = format!("{}.rs", path.display());
-                    if !fs::exists(&path_str).unwrap() {
-                        path_str = format!("{}/mod.rs", path.display());
-                    }
-
-                    let entry = files_for_structs.entry(path_str).or_default();
-                    entry.push(struct_name.to_string());
-                }
-            }
-        }
-
-        for ns in new_structs {
-            structs.push(ns);
-        }
-        if files_for_structs.is_empty() {
-            break;
-        }
-    }
-    */
 }
